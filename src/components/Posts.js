1301b2688edc2180cbfdba358beb0868802c738e
TamHOD <thomashodonnell@gmail.com>
2019-11-24 15:28:14 +0000

    use axios to communicate with server in posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 export const Posts = props => {
-  return <div>Posts go here!</div>;
+  useEffect(() => {
+    axios.get("/api/hello").then(res => setState(res.data));
+  }, []);
+
+  return (
+    <div>
+      Posts go here!
+      <p>{state}</p>
+    </div>
+  );
 };
 
 export default Posts;
