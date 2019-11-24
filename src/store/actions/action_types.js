2386b8a18b29c10af532d1f38e96da2c59f66e0d
TamHOD <thomashodonnell@gmail.com>
2019-11-24 10:02:09 +0000

    add login and input action types

--- src/store/actions/action_types.js
+++ src/store/actions/action_types.js
@@ -1,3 +1,11 @@
 export const SUCCESS = "SUCCESS";
 
 export const FAILURE = "FAILURE";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
+
+export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
