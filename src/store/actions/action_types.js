9ace2d8f20116565eb33c147bb760e41a76c6b6e
TamHOD <thomashodonnell@gmail.com>
2019-11-24 19:08:27 +0000

    add action types for profiles

--- src/store/actions/action_types.js
+++ src/store/actions/action_types.js
@@ -6,6 +6,10 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
+export const ADD_PROFILE = "ADD_PROFILE";
+
+export const REMOVE_PROFILE = "REMOVE_PROFILE";
+
 export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
 
 export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
